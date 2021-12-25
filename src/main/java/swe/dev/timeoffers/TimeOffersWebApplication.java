d0cd8e119f33ee2037c40822cfe14c7a8cd6fa3b
zynpgltkn <zeynepumargultekin@gmail.com>
2021-12-25 15:55:18 +0300

    Initial commit

---
+++ src/main/java/swe/dev/timeoffers/TimeOffersWebApplication.java
@@ -0,0 +1,13 @@
+package swe.dev.timeoffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeOffersWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeOffersWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swe/dev/timeoffers/TimeOffersWebApplicationTests.java
@@ -0,0 +1,13 @@
+package swe.dev.timeoffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeOffersWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
