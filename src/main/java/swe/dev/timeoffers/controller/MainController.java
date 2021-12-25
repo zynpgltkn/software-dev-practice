b28f538de8363d7e1af89cb332de6bb53c07ad20
zynpgltkn <zeynepumargultekin@gmail.com>
2021-12-25 18:44:32 +0300

    added welcomeprofile

--- src/main/java/swe/dev/timeoffers/controller/MainController.java
+++ src/main/java/swe/dev/timeoffers/controller/MainController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @RequestMapping("/")
     public String helloWorld(){
-            return "index";
+            return "welcomeprofile";
     }
 }
