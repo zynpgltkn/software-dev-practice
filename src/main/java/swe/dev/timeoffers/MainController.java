1c7cf48b171f3906cb5e7a1eadefe4fad128934a
zynpgltkn <zeynepumargultekin@gmail.com>
2021-12-25 17:07:02 +0300

    maincont

--- src/main/java/swe/dev/timeoffers/MainController.java
+++ src/main/java/swe/dev/timeoffers/MainController.java
@@ -1,4 +1,13 @@
 package swe.dev.timeoffers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+    @RequestMapping("/")
+    public String helloWorld(){
+            return "bla";
+    }
 }
